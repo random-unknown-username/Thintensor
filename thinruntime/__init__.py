16beb04c4507459372ac83a32b7b797ba3f76347
Satvik Hardat <satvikhardat@gmail.com>
2026-08-22 15:39:59 +0530

    Release Python runtime 1.0.2

--- thinruntime/__init__.py
+++ thinruntime/__init__.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 from importlib import import_module
 from typing import Any
 
-__version__ = "0.2.0"
+__version__ = "1.0.2"
 
 _EXPORTS = {
     "ThinArchive": (".archive", "ThinArchive"),
